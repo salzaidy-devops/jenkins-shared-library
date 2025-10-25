ee7a99cc9abdba0e6bfdadab3952e018843d9a2e
salzaidy <sh.alzaidy1@gmail.com>
2025-10-25 01:12:06 -0500

    parmatazied groovy script

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,14 @@
 #! /user/bin/env groovy
 
 
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh 'docker build -t salzaidy/coffee-api:1.0 .'
+        sh "docker build -t $imageName ."
+//        sh 'docker build -t salzaidy/coffee-api:3.2 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push salzaidy/coffee-api:1.0'
+        sh "docker push $imageName"
+//        sh 'docker push salzaidy/coffee-api:3.2'
     }
 }
 
