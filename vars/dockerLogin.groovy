6060111e6429c5db49a72bd454317bb9d3f25d8f
salzaidy <sh.alzaidy1@gmail.com>
2025-10-25 02:58:53 -0500

    split docker into stage

---
+++ vars/dockerLogin.groovy
@@ -0,0 +1,9 @@
+#! /user/bin/env groovy
+
+import com.example.Docker;
+
+def call() {
+    return new Docker(this).dockerLogin()
+}
+
+
---
+++ vars/dockerPush.groovy
@@ -0,0 +1,9 @@
+#! /user/bin/env groovy
+
+import com.example.Docker;
+
+def call(String imageName) {
+    return new Docker(this).dockerPush(imageName)
+}
+
+
