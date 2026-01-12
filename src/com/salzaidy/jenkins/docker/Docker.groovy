3596424eb32b8c4873b70f2cb5066e6b7e6e91ce
salzaidy <sh.alzaidy1@gmail.com>
2026-01-11 21:38:25 -0600

    fixed issue with ECR

--- src/com/salzaidy/jenkins/docker/Docker.groovy
+++ src/com/salzaidy/jenkins/docker/Docker.groovy
@@ -38,14 +38,36 @@ class Docker implements Serializable {
         }
     }
 
+
+
     private def ecrLogin() {
         def registry = script.env.DOCKER_ECR_REPO_SERVER
+        def region = script.env.AWS_REGION ?: 'us-east-1'
+
         if (!registry) {
             script.error("DOCKER_ECR_REPO_SERVER is not set")
         }
 
+        // Credential IDs are configurable, so Jenkinsfile stays provider-agnostic
+        def accessKeyIdCred = script.env.AWS_ACCESS_KEY_ID_CRED_ID ?: 'jenkins_aws_access_key_id'
+        def secretKeyCred   = script.env.AWS_SECRET_ACCESS_KEY_CRED_ID ?: 'jenkins_aws_secret_access_key'
+
         script.echo "Logging in to AWS ECR: ${registry}"
 
+        script.withCredentials([
+                script.string(credentialsId: accessKeyIdCred, variable: 'AWS_ACCESS_KEY_ID'),
+                script.string(credentialsId: secretKeyCred, variable: 'AWS_SECRET_ACCESS_KEY')
+        ]) {
+            // Optional: support session token if you ever use assumed roles
+            // If you have it, add another env var + credential and export AWS_SESSION_TOKEN.
+
+            script.sh """
+          aws ecr get-login-password --region ${region} \
+          | docker login --username AWS --password-stdin ${registry}
+        """
+        }
+
+        /*
         script.withCredentials([
                 script.usernamePassword(
                         credentialsId: 'ecr-credentials',
@@ -55,6 +77,8 @@ class Docker implements Serializable {
         ]) {
             script.sh "echo \"\$PASS\" | docker login -u \"\$USER\" --password-stdin ${registry}"
         }
+         */
+
     }
 
 
